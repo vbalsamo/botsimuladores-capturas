7dbee4082fc94bc86443de4dbc1d893c32e889bb
vlnhz <balvalentino2@gmail.com>
2023-02-22 10:16:37 -0300

    subtitulos gif

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def generate_thumbnail(in_filename, out_filename, duracion):
 
 def generate_gif(in_filename, out_filename, duracion):
     timess = random.uniform(1, duracion) #select random timestamp from video
-    os.system(f'ffmpeg -ss {timess} -t 3 -i {in_filename} -vf "fps=10,scale=600:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 {out_filename}')
+    os.system(f'ffmpeg -ss {timess} -t 3 -i {in_filename} -vf subtitles={nombreSubtitulos(in_filename)} "fps=10,scale=600:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 {out_filename}')
 
 def get_length(input_video):
     result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', input_video], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
