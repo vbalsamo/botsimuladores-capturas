806ed76e80dc73f40bb7fa8a99e88560265c47ee
vbalsamo <balvalentino@gmail.com>
2023-04-10 11:47:28 -0300

    aks

--- auth.py
+++ auth.py
@@ -7,7 +7,7 @@ accessKey = os.environ['ACCESS_TOKEN']
 accessSecret = os.environ['ACCESS_TOKEN_SECRET']
 
 def print_auth():
-    for q in (os.environ['API_KEY']):
+    for q in (os.environ['API_KEY_SECRET']):
           print(q)
 
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
